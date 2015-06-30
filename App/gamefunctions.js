442030463916db97c13fdabe869ba9b85bc0ebf7
Brendon Ho <brendonho01@users.noreply.github.com>
2015-06-30 10:37:08 -0400

    Update gamefunctions.js

--- App/gamefunctions.js
+++ App/gamefunctions.js
@@ -2,3 +2,14 @@
 var creator = "Brendon Ho";
 var developer = "Peter Soboyejo";
 var helper = "Prafull Sharma";
+
+//Player Attributes
+var gravity = 0.4;
+var positionX = 200;
+var positionY = 0;
+var velocityX += 1;
+var velocityY = 0;
+
+//Platforms
+
+
