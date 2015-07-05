c0a2c7e019f3c880db84f047677d08a1ec9dd5a8
Brendon Ho <brendonho01@users.noreply.github.com>
2015-07-05 19:44:25 -0400

    Update gamefunctions.swift

--- App/gamefunctions.swift
+++ App/gamefunctions.swift
@@ -2,3 +2,14 @@
 var creator = "Brendon Ho"
 var developer = "Peter Soboyejo"
 
+//random X and Y
+var randomX = random(50,255);
+var randomY = random(50,255);
+
+//platforms Width and Height
+var platformWidth = 100;
+var platformHeight = 10;
+
+//collide function
+
+
